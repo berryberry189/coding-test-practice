8b4c85a3f036af2117b164bf7c898e8a84706122
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-01-31 21:27:38 +0900

    Q2562 - 배열 - 최대값

---
+++ Coding_Test/src/com/grace/test/stack/Q2562.java
@@ -0,0 +1,22 @@
+package com.grace.test.stack;
+
+import java.io.*;
+public class Q2562 {
+	public static void main(String[] args) throws Exception {
+		// 최댓값 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int[] arr = new int[9];
+		int max = 0;
+		int cnt = 0;
+		for(int i=0; i<9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {
+				max = arr[i];
+				cnt = i+1;
+			}
+		}
+		bw.write(max+"\n"+cnt);
+		bw.flush();
+	}
+}
