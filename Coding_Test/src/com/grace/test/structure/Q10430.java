ae76ebeff15f32bada69f2ed319351f188a7b573
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-02-01 20:54:52 +0900

    Q10430 나머지

---
+++ Coding_Test/src/com/grace/test/structure/Q10430.java
@@ -0,0 +1,20 @@
+package com.grace.test.structure;
+
+import java.io.*;
+public class Q10430 {
+	public static void main(String[] args) throws Exception {
+		// 나머지
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] arr = br.readLine().split(" ");
+		int A = Integer.parseInt(arr[0]);
+		int B = Integer.parseInt(arr[1]);
+		int C = Integer.parseInt(arr[2]);
+		
+		bw.write((A+B)%C +"\n");
+		bw.write(((A%C) + (B%C))%C +"\n");
+		bw.write((A*B)%C +"\n");
+		bw.write(((A%C) * (B%C))%C +"\n");
+		bw.flush();
+	}
+}
