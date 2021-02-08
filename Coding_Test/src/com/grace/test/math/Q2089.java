1c2adbd8b1880ae47e750a4c0578fc56d0459809
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-02-08 17:02:24 +0900

    Q2089 add another code

--- Coding_Test/src/com/grace/test/math/Q2089.java
+++ Coding_Test/src/com/grace/test/math/Q2089.java
@@ -21,3 +21,33 @@ public class Q2089 {
 		}
 	}
 }
+/**
+public class Main {
+    public static void go(int n) {
+        if (n == 0) {
+            return;
+        }
+        if (n%2 == 0) {
+            go(-(n/2));
+            System.out.print(0);
+        } else {
+            if (n > 0) {
+                go(-(n/2));
+            } else {
+                go((-n+1)/2);
+            }
+            System.out.print(1);
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 0) {
+            System.out.println(0);
+        } else {
+            go(n);
+            System.out.println();
+        }
+    }
+}
+ **/
