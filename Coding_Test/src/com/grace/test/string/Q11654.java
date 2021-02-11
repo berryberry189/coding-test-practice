1f6975c7b83fe82ba821acb79ffadbaeeb313b62
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-02-11 21:40:00 +0900

    Q11654 아스키 코드

---
+++ Coding_Test/src/com/grace/test/string/Q11654.java
@@ -0,0 +1,9 @@
+package com.grace.test.string;
+
+public class Q11654 {
+	public static void main(String[] args) throws Exception{
+		// 아스키 코드
+		int input = System.in.read();
+		System.out.println(input);
+	}
+}
