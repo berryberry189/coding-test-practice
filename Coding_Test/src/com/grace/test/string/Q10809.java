8b0f1e913f5b6916a53ec66818e74748e6ea8ef3
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-02-12 15:25:44 +0900

    Q10809 알파벳 찾기

---
+++ Coding_Test/src/com/grace/test/string/Q10809.java
@@ -0,0 +1,25 @@
+package com.grace.test.string;
+
+import java.io.*;
+public class Q10809 {
+	public static void main(String[] args) throws Exception{
+		// 알파벳 찾기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int[] arr = new int[26];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = -1;
+		}
+		for(int i=0; i<input.length(); i++) {
+			char c = input.charAt(i);
+			if(arr[c -'a'] == -1) { // a = 97
+				arr[c -'a'] = i;
+			}
+		}
+		for(int val : arr) {
+			System.out.print(val + " ");
+		}
+		
+
+	}
+}
