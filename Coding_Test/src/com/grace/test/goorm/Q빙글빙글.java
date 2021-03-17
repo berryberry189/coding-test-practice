6d66695d71c49c1056fc201b7facceb29ccef198
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-03-17 14:16:22 +0900

    fix goorm 달팽이

--- Coding_Test/src/com/grace/test/goorm/Q빙글빙글.java
+++ Coding_Test/src/com/grace/test/goorm/Q빙글빙글.java
@@ -9,12 +9,42 @@ public class Q빙글빙글 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[][] arr = new int[n][n];
-		int num = 0;
-		int x=0, y=0, width = n, height = n-1;
+		String[][] arr = new String[n][n];
+		int x = 0, cnt = 1, max = n-1;
+		while(cnt <= n) {
+			// 오른쪽
+			for(int i=(x==0? 0 :x-1); i<=max; i++) {
+				arr[x][i] = "#";
+			}
+			cnt++; if(cnt == n+1) break;
+			
+			// 아래쪽
+			for(int i=x; i<=max; i++) {
+				arr[i][max] = "#";
+			}
+			cnt++; if(cnt == n+1) break;
+			
+			// 왼쪽
+			for(int i=max; i>=x; i--) {
+				arr[max][i] = "#";
+			}
+			cnt++; if(cnt == n+1) break;
+			
+			// 윗쪽
+			for(int i=max; i>=x+2; i--) {
+				arr[i][x] = "#";
+			}
+			cnt++; if(cnt == n+1) break;
+			x += 2;
+			max -= 2;
+		}
 		
-		
-		System.out.println();
+		for(int i = 0; i<arr.length;i++){        
+            for(int j =0; j< arr[i].length;j++){
+                System.out.print(arr[i][j] == null ? " ": arr[i][j]);                        
+            }
+            System.out.println();
+        }      
 		
 	}
 }
