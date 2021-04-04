1beb2eb68d3552df0ccae84c8262e499952216ec
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-04-04 15:52:11 +0900

    [프로그래머스] 캐시

---
+++ Coding_Test/src/com/grace/test/programmers/LRU_캐시.java
@@ -0,0 +1,44 @@
+package com.grace.test.programmers;
+
+import java.util.LinkedList;
+
+public class LRU_캐시 {
+	
+	public static void main(String[] args) {
+		System.out.println(Solution());
+	}
+
+	static int Solution() {
+		// TODO Auto-generated method stub
+		int answer = 0;
+		int cacheSize = 5;
+		String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+		int CACHE_HIT = 1;
+        int CACHE_MISS = 5;
+        
+        if(cacheSize == 0) {
+        	return CACHE_MISS* cities.length;
+        }
+        
+        LinkedList<String> cache = new LinkedList<String>();
+        
+        for(String item : cities) {
+        	String city = item.toLowerCase();
+        	int idx = cache.indexOf(city);
+        	// hit
+        	if(idx != -1) {
+        		answer += CACHE_HIT;
+        		cache.remove(idx);
+        		cache.add(city);
+    		// miss
+        	}else {
+        		answer += CACHE_MISS;
+        		if(cache.size() >= cacheSize) cache.removeFirst();
+        		cache.add(city);
+        	}
+        }
+       return answer;
+
+	}
+
+}
