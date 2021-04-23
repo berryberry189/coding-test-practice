d081f0f758f29961feed3491db4ee356fb702547
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-04-23 11:26:58 +0900

    [BOJ 4963] 섬의개수 ing

---
+++ Coding_Test/src/com/grace/test/dfsbfs/Q4963_섬의개수.java
@@ -0,0 +1,23 @@
+package com.grace.test.dfsbfs;
+
+class Pair {
+    int x;
+    int y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Q4963_섬의개수 {
+	
+	static final int[] dx = {0, 0, 1, -1};
+    static final int[] dy = {1, -1, 0, 0};
+    
+	public static void main(String[] args) {
+		// 인접한 방향이 대각선이 추가된 경우
+		
+	    
+
+	}
+}
