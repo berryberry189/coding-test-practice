9bd7035a065a13a7cbc32598c5d51d32faa9a9f0
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-04-30 16:54:45 +0900

    [BOJ 1991] 트리순회 ing

---
+++ Coding_Test/src/com/grace/test/tree/Q1991_트리순회.java
@@ -0,0 +1,20 @@
+package com.grace.test.tree;
+
+import java.io.*;
+import java.util.*;
+class Node{
+	int left, right;
+	Node(int left, int right){
+		this.left = left;
+		this.right = right;
+	}
+}
+public class Q1991_트리순회 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		
+
+	}
+}
