17f8ed48c790ac81e82c9b105553b0d2d9bd9e23
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-05-25 23:11:20 +0900

    [programmers] 순위검색

--- Coding_Test/src/com/grace/test/programmers/Q순위검색.java
+++ Coding_Test/src/com/grace/test/programmers/Q순위검색.java
@@ -3,11 +3,11 @@ package com.grace.test.programmers;
 import java.util.*;
 
 public class Q순위검색 {
-	static Map<String, Integer> wordMap = new HashMap<>();
-	static List<List<Integer>> scoreList = new ArrayList<>();
+
 	public static void main(String[] args) {
 		// https://programmers.co.kr/learn/courses/30/lessons/72412
-		// 참고 https://www.youtube.com/watch?v=eBQtFteduyw&t=873s
+		// 참고   https://www.youtube.com/watch?v=eBQtFteduyw&t=873s
+		//     https://girawhale.tistory.com/94
 		String[] info = {"java backend junior pizza 150","python frontend senior chicken 210",
 							"python frontend senior chicken 150","cpp backend senior pizza 260",
 							"java backend junior chicken 80","python backend senior chicken 50"};
@@ -15,77 +15,64 @@ public class Q순위검색 {
 		String[] query = {"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
 						  "cpp and - and senior and pizza 250","- and backend and senior and - 150",
 						  "- and - and - and chicken 100","- and - and - and - 150"};
-	
+	   
+		int[] result = Solution.solution(info,query);
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
 		
-		//public static int[] solution(String[] info, String[] query) {
-			
-			wordMap.put("-", 0);
-			wordMap.put("cpp", 1);
-			wordMap.put("java", 2);
-			wordMap.put("phython", 3);
-			wordMap.put("backend", 1);
-			wordMap.put("frontend", 2);
-			wordMap.put("junior", 1);
-			wordMap.put("senior", 2);
-			wordMap.put("chicken", 1);
-			wordMap.put("pizza", 2);
-			
-			for(int i=0; i<4*3*3*3; i++) {
-				scoreList.add(new ArrayList<>());
-			}
-			
+	
+	}
+	static class Solution {	
+		public static int[] solution(String[] info, String[] query) {
+			Map<String, List<Integer>> map = new HashMap<>();
 			for(String str : info) {
-				String[] word = str.split(" ");
-				int[] arr = {wordMap.get(word[0])*3*3*3,
-							 wordMap.get(word[1])*3*3,
-							 wordMap.get(word[2])*3,
-							 wordMap.get(word[3])};
-				int score = Integer.parseInt(word[4]);
+				String[] arr = str.split(" ");
+				int score = Integer.parseInt(arr[4]);
 				
-				for(int i=0; i<(1<<4); i++) {
-					int idx = 0;
+				for(int i=0; i<(1<<4); i++) { // 1<<4 = 1 * 2^4 
+					StringBuilder key = new StringBuilder();
 					for(int j=0; j<4; j++) {
-						if((i & (1<<j)) != 0) {
-							idx += arr[j];
-						}
+						if((i & (1 << j)) > 0) key.append(arr[j]);
 					}
-					scoreList.get(idx).add(score);
+					// Value value = map.computeIfAbsent(key, k -> getNewValue(key));
+					// Key 값에 해당하는 Value가 존재하면 가져와서 사용하고 없으면 새로 만들어줌
+					// => value가 없다면 새로운 ArrayList를 생성하고 생성한 List에 점수를 넣어주기
+					map.computeIfAbsent(key.toString(), s -> new ArrayList<>()).add(score);
 				}
 			}
-			for(int i=0; i<4*3*3*3; i++) {
-				Collections.sort(scoreList.get(i));
-			}
 			
+			List<Integer> empty = new ArrayList<>();
+			
+		
+			// entrySet : map에 저장된 key-value값을 엔트리(키와 값 결합)의 형태로 set에 저장하여 반환
+	        for (Map.Entry<String, List<Integer>> entry : map.entrySet())
+	        	// list 정렬
+	            entry.getValue().sort(null);
+
 	        int[] answer = new int[query.length];
-	        
-	        for(int i=0; i<query.length; i++) {
-	        	String[] word = query[i].split(" ");
-	        	int idx = wordMap.get(word[0]) *3*3*3 + 
-	        			wordMap.get(word[2]) *3*3 +
-	        			wordMap.get(word[4])*3 +
-	        			wordMap.get(word[6]);
-	        	int score = Integer.parseInt(word[7]);
-	        	int ret = Collections.binarySearch(scoreList.get(idx),score);
-	        	if(ret<0) {
-	        		ret = -(ret +1);
-	        	}else {
-	        		for(int j=ret-1; j>=0; j--){
-	        			if(scoreList.get(idx).get(j) == score) {
-	        				ret = j;
-	        			}else {
-	        				break;
-	        			}
-	        		}
-	        	}
-	        	answer[i] = scoreList.get(idx).size() - ret;
+	        for (int i = 0; i < query.length; i++) {
+	            String[] split = query[i].replaceAll("-", "").split(" and | ");
+	            String key = String.join("", split[0], split[1], split[2], split[3]);
+	            int score = Integer.parseInt(split[4]);
+	           
+	            // getOrDefault : 찾는 키가 존재하면 키의 값 반환, 없으면 기본값(아래의 empty) 반환
+	            List<Integer> list = map.getOrDefault(key, empty);
+
+	            int s = 0, e = list.size();
+
+	            while (s < e) {
+	                int mid = (s + e) / 2;
+
+	                if (list.get(mid) < score) s = mid + 1;
+	                else e = mid;
+	            }
+
+	            answer[i] = list.size() - s;
 	        }
-	        
-	        for(int i=0; i<answer.length; i++) {
-	        	System.out.println(answer[i]);
-	        }
-	        
-	        //return answer;
-	   // }
+
+	        return answer;
+	   }
 		
 	}
 }
