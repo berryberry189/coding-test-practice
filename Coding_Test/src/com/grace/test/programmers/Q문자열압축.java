f6627f12d9261474084fd161863dfa5b6c01a8b5
berryberry189 <berryberry189@naver.com>
2021-09-12 23:26:30 +0900

    [programmers] 문자열압축

---
+++ Coding_Test/src/com/grace/test/programmers/Q문자열압축.java
@@ -0,0 +1,42 @@
+package com.grace.test.programmers;
+
+public class Q문자열압축 {
+
+    public static void main(String[] args) {
+        System.out.println("Answer: " + solution("ababababc"));
+    }
+
+    public static int solution(String s){
+        int min = s.length();
+        if(s.length() == 1) return 1;
+
+        for(int i = 1; i<=s.length()/2; i++){
+            StringBuilder sb= new StringBuilder();
+            String temp = "";
+            int cnt = 0;
+
+            for(int j=0; j<Math.ceil((double)s.length()/i); j++){
+                String val = s.substring(j*i, i*(j+1) >= s.length() ? s.length() : i*(j+1));
+                if(!temp.equals(val)) {
+                    if(cnt != 1) {
+                        sb.append(cnt);
+                        cnt = 1;
+                    }
+                    sb.append(temp);
+                    temp = val;
+                }
+                else{
+                    cnt ++;
+                }
+            }
+            if(cnt != 1) sb.append(cnt);
+            sb.append(temp);
+
+            min = Math.min(min, sb.toString().length());
+        }
+        return min;
+    }
+
+}
+
+
