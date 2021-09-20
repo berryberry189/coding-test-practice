cf8f4031c332d627bb147420c3ca729b5cee9c02
berryberry189 <berryberry189@naver.com>
2021-09-20 21:10:27 +0900

    [leetcode] sql 180

--- Coding_Test/WebContent/sql/leetcode_medium.sql
+++ Coding_Test/WebContent/sql/leetcode_medium.sql
@@ -15,3 +15,22 @@ END
 SELECT score, DENSE_RANK() OVER (ORDER BY Score DESC) AS 'Rank'
 FROM Scores
 
+
+-- https://leetcode.com/problems/consecutive-numbers/submissions/
+-- Consecutive Numbers
+-- 느림
+SELECT distinct a.num as ConsecutiveNums
+FROM logs a
+INNER JOIN logs b
+ON a.id + 1 = b.id AND a.num = b.num
+INNER JOIN logs c
+ON a.id + 2 = c.id AND a.num = c.num;
+
+-- 빠름
+SELECT DISTINCT L.Num as ConsecutiveNums
+FROM(
+    SELECT Id, Num
+         , Lag(Num) OVER (ORDER BY Id) as 'Lag'    -- Num - 1
+         , Lead(Num) OVER (ORDER By Id) as 'Lead'  -- Num + 1
+    FROM Logs) L
+WHERE L.Num = L.Lag AND L.Lag = L.Lead
