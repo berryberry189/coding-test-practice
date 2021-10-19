6d78b06c184d36dc47d5c11cdc321e7327bf7c61
berryberry189 <berryberry189@naver.com>
2021-10-19 20:28:12 +0900

    [leetcode] sql 601

--- Coding_Test/WebContent/sql/leetcode_hard.sql
+++ Coding_Test/WebContent/sql/leetcode_hard.sql
@@ -21,3 +21,18 @@ WHERE (Request_at BETWEEN '2013-10-01' AND '2013-10-03')
 AND Client_id NOT IN (SELECT Users_Id FROM Users WHERE Banned = 'Yes')
 AND Driver_Id NOT IN (SELECT Users_Id FROM Users WHERE Banned = 'Yes')
 GROUP BY Request_at;
+
+
+-- 601. Human Traffic of Stadium
+-- https://leetcode.com/problems/human-traffic-of-stadium/
+SELECT distinct t1.*
+from Stadium t1, Stadium t2, Stadium t3
+WHERE t1.people >= 100 AND t2.people >= 100 AND t3.people >= 100
+AND(
+    (t1.id - t2.id = 1 and t1.id - t3.id = 2 and t2.id - t3.id =1)  -- t1, t2, t3
+    or
+    (t2.id - t1.id = 1 and t2.id - t3.id = 2 and t1.id - t3.id =1) -- t2, t1, t3
+    or
+    (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) -- t3, t2, t1
+)
+order by t1.id
