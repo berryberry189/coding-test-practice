ebc7bd798864695c09eff6c76384801bf627549a
berryberry189 <berryberry189@naver.com>
2022-01-07 22:09:39 +0900

    [programmers] Level1 숫자문자열과영단어

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_숫자문자열과영단어.java
@@ -0,0 +1,24 @@
+package com.grace.test.programmers;
+
+public class Level1_숫자문자열과영단어 {
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s) {
+
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i=0; i<arr.length; i++){
+            if(s.contains(arr[i])){
+                s = s.replace(arr[i], String.valueOf(i));
+            }
+        }
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
