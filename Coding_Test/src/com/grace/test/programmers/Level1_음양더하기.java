e9039485eaa6238f132bb7a104925f8af68f3b7e
berryberry189 <berryberry189@naver.com>
2022-01-14 23:20:25 +0900

    [programmers] LEVEL1 음양더하기

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_음양더하기.java
@@ -0,0 +1,26 @@
+package com.grace.test.programmers;
+
+public class Level1_음양더하기 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/76501
+
+    public static void main(String[] args) {
+        int[] absolute = {4,7,12};
+        boolean[] signs = {true,false,true};
+        System.out.println(solution(absolute, signs));
+    }
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i=0; i< absolutes.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer += absolutes[i]*-1;
+            }
+        }
+
+        return answer;
+    }
+}
