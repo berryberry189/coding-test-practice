8c277e22db8ba499ad2a22d2527834d9dc70c96b
berryberry189 <berryberry189@naver.com>
2022-01-14 23:13:49 +0900

    [programmers] LEVEL1 없는숫자더하기

---
+++ Coding_Test/src/com/grace/test/programmers/LEVEL1_없는숫자더하기.java
@@ -0,0 +1,20 @@
+package com.grace.test.programmers;
+
+public class LEVEL1_없는숫자더하기 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/86051
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(int[] numbers) {
+        int answer = 45;
+        for (int number : numbers) {
+                answer -= number;
+        }
+        return answer;
+    }
+
+}
