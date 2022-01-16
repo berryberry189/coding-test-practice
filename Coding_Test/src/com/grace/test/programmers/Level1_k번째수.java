264c7d98966c7c7c4c353ffb682879089f116c72
berryberry189 <berryberry189@naver.com>
2022-01-16 19:36:14 +0900

    [programmers] Level1 k번째수

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_k번째수.java
@@ -0,0 +1,26 @@
+package com.grace.test.programmers;
+
+import java.util.Arrays;
+
+public class Level1_k번째수 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/70128
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(solution(array, commands));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+
+        }
+        return answer;
+    }
+
+}
