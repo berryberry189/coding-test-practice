0d7eb83ccf11ae2114d34f6c976cfcea6614a701
berryberry189 <berryberry189@naver.com>
2022-01-24 22:03:04 +0900

    [programmers] Level1 폰켓몬

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_폰켓몬.java
@@ -0,0 +1,32 @@
+package com.grace.test.programmers;
+
+import java.util.HashSet;
+
+public class Level1_폰켓몬 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/1845
+
+    public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2,4};
+        System.out.println(solution(nums));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+        int num = nums.length / 2;
+
+        HashSet<Integer> numsSet = new HashSet<>();
+        for (int n : nums) {
+            numsSet.add(n);
+        }
+
+        if (numsSet.size() > num) {
+            answer = num;
+        } else {
+            answer = numsSet.size();
+        }
+
+        return answer;
+    }
+
+}
