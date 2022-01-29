dc5f64d07bb32319bedc5cddbdb27a836ab8ee95
berryberry189 <berryberry189@naver.com>
2022-01-29 20:56:24 +0900

    [programmers] Level1 약수의개수와덧셈

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_약수의개수와덧셈.java
@@ -0,0 +1,26 @@
+package com.grace.test.programmers;
+
+public class Level1_약수의개수와덧셈 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/77884
+
+    public static void main(String[] args) {
+        int left = 13;
+        int right = 17;
+        System.out.println(solution(left, right));
+    }
+
+    public static int solution(int left, int right) {
+        int answer = 0;
+        int cnt = 1;
+        for(int i=left; i<=right; i++){
+            for(int j=1; j<=i/2; j++){
+                if(i%j == 0) cnt++;
+            }
+            answer += (cnt%2 == 0 ? i : i * -1);
+            cnt = 1;
+        }
+        return answer;
+    }
+
+}
