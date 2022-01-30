934fd417b4d0f2b68518e79505c7319d42ee0869
berryberry189 <berryberry189@naver.com>
2022-01-30 20:15:22 +0900

    [programmers] Level1 예산

---
+++ Coding_Test/src/com/grace/test/programmers/Level1_예산.java
@@ -0,0 +1,30 @@
+package com.grace.test.programmers;
+
+import java.util.Arrays;
+
+public class Level1_예산 {
+
+    // https://programmers.co.kr/learn/courses/30/lessons/12982
+
+    public static void main(String[] args) {
+        int[] d = {2,2,3,3};
+        int budget = 10;
+        System.out.println(solution(d, budget));
+    }
+
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        int sum = 0;
+        for (int i : d) {
+            sum += i;
+            if(sum <= budget){
+                answer++;
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+
+}
