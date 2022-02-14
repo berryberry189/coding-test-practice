63f44db886b7f9d470815c9452106d207ec151d4
berryberry189 <berryberry189@naver.com>
2022-02-14 22:15:48 +0900

    [programmers] Level1 나머지가1이되는수찾기

---
+++ Coding_Test/src/com/grace/test/programmers/level1/Level1_나머지가1이되는수찾기.java
@@ -0,0 +1,20 @@
+package com.grace.test.programmers.level1;
+
+public class Level1_나머지가1이되는수찾기 {
+    // https://programmers.co.kr/learn/courses/30/lessons/87389
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(solution(n));
+
+    }
+    public static int solution(int n) {
+        int min = 0;
+        for(int i=1; i<n; i++){
+            if(n%i == 1){
+                min = i;
+                break;
+            }
+        }
+        return min;
+    }
+}
