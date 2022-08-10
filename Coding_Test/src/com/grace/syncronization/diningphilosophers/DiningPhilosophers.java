0eeb05c068df0a5f3319417d80b934c632a2be73
berryberry189 <berryberry189@naver.com>
2022-08-10 22:22:54 +0900

    철학자들의 저녁 식사 문제

--- Coding_Test/src/com/grace/syncronization/diningphilosophers/DiningPhilosophers.java
+++ Coding_Test/src/com/grace/syncronization/diningphilosophers/DiningPhilosophers.java
@@ -3,8 +3,10 @@ package com.grace.syncronization.diningphilosophers;
 public class DiningPhilosophers {
 
     public static void main(String[] args) throws Exception {
+
         int numOfPhils = 5; // 철학자 인원
         Philosopher[] philosophers = new Philosopher[numOfPhils];
+
         // 모니터 생성
         DiningPhilosopherMonitor monitor = new DiningPhilosopherMonitor(numOfPhils);
         for (int i = 0; i < philosophers.length; i++)
