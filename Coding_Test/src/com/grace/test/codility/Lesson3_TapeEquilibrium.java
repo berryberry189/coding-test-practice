8fc184e64132091fa6feaa8e47449c43f306997a
berryberry189 <berryberry189@naver.com>
2023-01-11 22:07:21 +0900

    [codility] Lesson3_TapeEquilibrium

---
+++ Coding_Test/src/com/grace/test/codility/Lesson3_TapeEquilibrium.java
@@ -0,0 +1,25 @@
+package com.grace.test.codility;
+
+import java.util.Arrays;
+
+public class Lesson3_TapeEquilibrium {
+
+  // https://app.codility.com/c/run/training6PTEGT-49Z/
+
+  public static void main(String[] args) {
+    System.out.println("test = " + solution(new int[]{3,1,2,4,3}));
+  }
+
+  static  int solution(int[] A) {
+    int len = A.length;
+    int sum = Arrays.stream(A).sum();
+    int left = 0;
+    int min = Integer.MAX_VALUE;
+    for(int i=0; i<len; i++) {
+      left += A[i];
+      min = Math.min(min, Math.abs(left - (sum - left)));
+    }
+    return min;
+  }
+
+}
