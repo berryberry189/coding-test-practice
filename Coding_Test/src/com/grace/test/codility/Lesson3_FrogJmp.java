1dd07a94f614ea0cb3ef4ea8ee6c5123aaa059db
berryberry189 <berryberry189@naver.com>
2023-01-11 21:10:04 +0900

    [codility] Lesson3_FrogJmp

---
+++ Coding_Test/src/com/grace/test/codility/Lesson3_FrogJmp.java
@@ -0,0 +1,21 @@
+package com.grace.test.codility;
+
+public class Lesson3_FrogJmp {
+  // https://app.codility.com/c/run/training8WZFHC-C7D/
+
+  public static void main(String[] args) {
+
+    System.out.println("test = " + solution(10, 85, 30));
+
+  }
+
+  static int solution(int X, int Y, int D) {
+    // D * result >= Y - X
+    // result > = (Y - X)/D
+    int result = (Y-X)/D;
+    if((Y-X) == D * result) return result;
+    return result + 1;
+
+  }
+
+}
