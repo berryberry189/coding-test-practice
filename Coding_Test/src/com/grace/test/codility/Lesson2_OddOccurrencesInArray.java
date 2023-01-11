b8cca520073653ac292aaa277a1fc1bce0a33fe5
berryberry189 <berryberry189@naver.com>
2023-01-11 20:37:17 +0900

    [codility] Lesson2_OddOccurrencesInArray

---
+++ Coding_Test/src/com/grace/test/codility/Lesson2_OddOccurrencesInArray.java
@@ -0,0 +1,26 @@
+package com.grace.test.codility;
+
+import java.util.HashSet;
+
+public class Lesson2_OddOccurrencesInArray {
+
+  public static void main(String[] args) {
+
+    System.out.println("test = " + solution(new int[]{9, 3, 9, 3, 9, 7, 9}));
+    System.out.println("test = " + solution(new int[]{5, 5, 7, 7, 8}));
+
+  }
+
+  static int solution(int[] A) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0; i<A.length; i++) {
+      if(set.contains(A[i])){
+        set.remove(A[i]);
+      } else {
+        set.add(A[i]);
+      }
+    }
+    return set.stream().iterator().next();
+  }
+
+}
