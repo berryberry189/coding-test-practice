6a10492bac6fd0c189ec1d62f0860052ffe61f26
berryberry189 <berryberry189@naver.com>
2023-01-14 16:00:28 +0900

    [codility] Lesson6_Distinct

---
+++ Coding_Test/src/com/grace/test/codility/Lesson6_Distinct.java
@@ -0,0 +1,21 @@
+package com.grace.test.codility;
+
+import java.util.Arrays;
+
+public class Lesson6_Distinct {
+
+  public static void main(String[] args) {
+
+    System.out.println("Distinct : " + solution(new int[]{2, 1, 1, 2, 3, 1}));
+  }
+
+  private static int solution(int[] A) {
+    A = Arrays.stream(A)
+        .distinct()
+        .toArray();
+
+    return A.length;
+
+  }
+
+}
