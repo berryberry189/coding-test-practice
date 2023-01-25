1d2af8fbf385fb8e965ca94d414d17dfc6b5822c
berryberry189 <berryberry189@naver.com>
2023-01-25 23:44:00 +0900

    [inflearn] Stack Queue 괄호문자제거

--- Coding_Test/src/com/grace/test/inflearn/stack_queue/Section5_StackQueue.java
+++ Coding_Test/src/com/grace/test/inflearn/stack_queue/Section5_StackQueue.java
@@ -10,6 +10,8 @@ public class Section5_StackQueue {
     System.out.println("Q1. 올바른 괄호: " + solution1("(()(()))(()"));
     System.out.println("Q1. 올바른 괄호: " + solution1("(())()"));
 
+    // Q2. 괄호문자제거
+    System.out.println("Q2. 괄호문자제거: " + solution2("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
 
   }
 
@@ -29,6 +31,25 @@ public class Section5_StackQueue {
     }
     if(!stack.isEmpty()) answer = "NO";
     return answer;
+  }
+
+  private static String solution2(String str) {
+    StringBuilder sb = new StringBuilder();
+    Stack<Character> stack = new Stack<>();
+    for(char c : str.toCharArray()) {
+      if(c != ')') {
+        stack.push(c);
+      } else {
+        if(!stack.isEmpty()) {
+          while (stack.pop() != '(') {}
+        }
+      }
+    }
+
+    for(char c : stack) {
+      sb.append(c);
+    }
+    return sb.toString();
   }
 
 }
